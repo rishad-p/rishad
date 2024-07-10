8f807f87cf396eaf65c458ddfeca1d19e42e4c6d
rishad <rishadpulikkodan@gmail.com>
2024-07-10 17:37:59 +0530

    _

--- scripts.js
+++ scripts.js
@@ -32,18 +32,16 @@ function load(){
         $("#boot_bg").css("top","10vh");
         $("#boot_bg").css("right","10vh");
         $("#boot_bg").css("border-radius", "20px");
-		$("#boot_bg").css("backdrop-filter", "blur(10px)");
+		// $("#boot_bg").css("backdrop-filter", "blur(10px)");
 		$("#boot_bg").attr("onclick", "menu_toggle()");
 		setTimeout(() =>{
 			clearInterval(boot);
 			$(".cir").css("animation", "none");
 			$(".cir").css("border-radius", "100px");
-
 			$("#cir1").css("width", "60px");
 			$("#cir1").css("height", "10px");
 			$("#cir4").css("width", "60px");
 			$("#cir4").css("height", "10px");
-
 			$("#boot_bg").css("background", "#ffffff40");
 			$("#boot_bg").css("width", "80px");
 			$("#boot_bg").css("height", "80px");
@@ -64,11 +62,9 @@ function menu_toggle(){
 		$("#boot_bg").css("height", "100vh");
 		$("#boot_bg").css("top", "10vh");
 		$("#boot_bg").css("right", "10vh");
-
 		$("#close").css("width", "70px");
 		$("#close").css("height", "70px");
 		$("#close").css("box-shadow", "0px 0px 0px 4px yellow");
-
 		$(".cir").css("width", "250px");
 		$(".cir").css("height", "50px");
 		$(".cir").css("margin", "20px");
@@ -80,9 +76,6 @@ function menu_toggle(){
 			$("#boot_bg").css("top", "0px");
 			$("#boot_bg").css("right", "0px");
 			$("#boot_bg").css("background", "#00000047");
-
-
-
 			$(".cir").css("background", "transparent");
 			$(".cir").css("box-shadow", "0px 0px 0px 4px yellow");
 
@@ -92,7 +85,6 @@ function menu_toggle(){
 		$("#boot_bg").attr("data", "off");
 		$("#boot_bg").css("top", "10vh");
 		$("#boot_bg").css("right", "10vh");
-
 		$("#rishad").css("transform", "scale(0%)");
 		setTimeout(() =>{
 			$("#boot_bg").attr("onclick", "menu_toggle()");
@@ -102,11 +94,9 @@ function menu_toggle(){
 			$("#boot_bg").css("top", "10px");
 			$("#boot_bg").css("right", "10px");
 			$("#boot_bg").css("background", "#ffffff40");
-
 			$("#close").css("width", "0px");
 			$("#close").css("height", "0px");
 			$("#close").css("box-shadow", "none");
-
 			$("#cir1").css("width", "60px");
 			$("#cir1").css("height", "10px");
 			$("#cir2").css("width", "0px");
